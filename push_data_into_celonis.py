8224f1ca0a10780c11497dd9462385c8a7fd0749
Darryl Lardizabal <dsteven12@gmail.com>
2021-02-15 12:30:26 -0800

    Feature: Added Progress bar added to terminal for progress status of table creation.

--- push_data_into_celonis.py
+++ push_data_into_celonis.py
@@ -1,12 +1,15 @@
 from pycelonis import get_celonis
 import csv_to_cleaned_df as clean_df
 import config as c
+from rich.console import Console
+from time import sleep
 
 # Global Variables and Envorinment Variables
 connection_settings = {
     "celonis_url": c.API_URL,
     "api_token": c.API_TOKEN,
 } 
+console = Console()
 
 # Connects to Celonis IBC
 celonis = get_celonis(**connection_settings)
@@ -16,7 +19,10 @@ data_pool_id = "6b43443e-0cee-40bc-90fb-e941e42f41f8"
 data_pool = celonis.pools.find(data_pool_id)
 
 # Create Table in the data_pool based on Claims Data
-create_table = data_pool.push_table(table_name='Claims', df_or_path=clean_df.df3, if_exists='replace')
-
-# Checks if table has been created
-print(data_pool.tables)
+with console.status("[bold green]Pushing data...") as status:
+    create_table = data_pool.push_table(table_name='Claims', df_or_path=clean_df.df3, if_exists='replace')
+    sleep(1)
+    console.log(f"[green]Finished pushing data[/green]")
+    console.log(f'[bold][red]Done!')
+    #Validates table was created
+    console.log(f'[bold][blue]Validating table: [green]{data_pool.tables}')

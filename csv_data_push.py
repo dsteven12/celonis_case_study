64b23314dc4a9a27f09211e0295bef39375d6ad1
Darryl Lardizabal <dsteven12@gmail.com>
2021-02-16 15:16:37 -0800

    Refactor: Updated jobs to be included in status check.

--- csv_data_push.py
+++ csv_data_push.py
@@ -10,14 +10,31 @@ console = Console()
 csvUploadFile = ccd.updated_file # Path to csv file you want to upload
 
 cloud = cloud_api.cloud(c.TENANT, c.REALM, c.TOKEN)
-new_job = cloud.create_job(c.POOL_ID, "CLAIMS_DATA")
-new_job_id = new_job["id"]
+
 
 # Provides console status for pushing data chunks and submitting of jobs
-with console.status("[bold green]Uploading Data...") as status:
-    push_chunk = cloud.push_new_chunk(c.POOL_ID, new_job_id, csvUploadFile)
-    console.log(f"[green]Finished pushing data! {push_chunk}[/green]")
+with console.status("[bold][green]Uploading Data into Celonis...") as status:
+    # Create Job
+    new_job = cloud.create_job(c.POOL_ID, "CLAIMS_DATA")
+    new_job_id = new_job["id"]
+    console.log(f"[green]Finished creating job! {new_job}[/green]")
     sleep(1)
+    # Push Data Chunks
+    push_chunk = cloud.push_new_chunk(c.POOL_ID, new_job_id, csvUploadFile)
+    console.log(f"[green]Finished pushing data chunk! {push_chunk}[/green]")
+    sleep(1)
+    # Submit Job
     submit_job = cloud.submit_job(c.POOL_ID, new_job_id)
     console.log(f"[green]Finished submitting job! {submit_job}[/green]")
-    console.log(f'[bold][red]Done!')  
+    sleep(1)
+    # List Jobs
+    list_job = cloud.list_jobs(c.POOL_ID)
+    for job in list_job[:3]:
+        console.log(f"[blue]Data_Pool_ID: {job['dataPoolId']}") 
+        console.log(f"[blue]JOB_ID: {job['id']}") 
+        console.log(f"[blue]JOB_STATUS: {job['status']}") 
+        console.log(f"[blue]JOB_LOGS: {job['logs']}")
+    console.log(f"[green]Finished list jobs for status check![/green]")
+    sleep(1)
+    console.log(f'[bold][red]Done!') 
+

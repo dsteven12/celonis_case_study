478d363a81206a8e854a594566eb62b603ec1ffb
Darryl Lardizabal <dsteven12@gmail.com>
2021-02-18 11:43:36 -0800

    Refactor: Refactored color scheme for console.

--- csv_data_push.py
+++ csv_data_push.py
@@ -17,34 +17,34 @@ def list_recent_job(job_id: str, pool_id: str) -> None:
     """
     for job in cloud.list_jobs(pool_id):
         if job_id == job['id']: 
-            console.log(f"[yellow]JOB_ID: {job['id']}") 
+            console.log(f"[blue]JOB_ID: [bold yellow]{job['id']}") 
             console.log(f"[blue]TARGET_NAME: {job['targetName']}") 
-            console.log(f"[yellow]JOB_STATUS: {job['status']}") 
+            console.log(f"[blue]JOB_STATUS: {job['status']}") 
             console.log(f"[blue]FILE_TYPE: {job['fileType']}")
             console.log(f"[blue]JOB_TYPE: {job['type']}")
-            console.log(f"[yellow]JOB_LOGS: {job['logs']}")
+            console.log(f"[blue]JOB_LOGS: [bold yellow]{job['logs']}")
             console.log(f"[blue]DATA_POOL_ID: {job['dataPoolId']}") 
     
 
 # Provides console status for pushing data chunks and submitting of jobs
-with console.status("[bold][green]Uploading Data into Celonis...") as status:
+with console.status("[bold green]Uploading Data into Celonis...") as status:
     # Create Job
     new_job = cloud.create_job(c.POOL_ID, "CLAIMS_DATA")
     new_job_id = new_job["id"]
-    console.log(f"New job id: {new_job_id}")
-    console.log(f"[green]Finished creating job! {new_job}[/green]")
+    console.log(f"New job id: [bold yellow]{new_job_id}")
+    console.log(f"[magenta]Finished creating job! [green]{new_job}")
     sleep(1)
     # Push Data Chunks
     push_chunk = cloud.push_new_chunk(c.POOL_ID, new_job_id, csvUploadFile)
-    console.log(f"[green]Finished pushing data chunk! {push_chunk}[/green]")
+    console.log(f"[magenta]Finished pushing data chunk! [bold green]{push_chunk}")
     sleep(1)
     # Submit Job
     submit_job = cloud.submit_job(c.POOL_ID, new_job_id)
-    console.log(f"[green]Finished submitting job! {submit_job}[/green]")
+    console.log(f"[magenta]Finished submitting job! [bold green]{submit_job}")
     sleep(1)
     # List Jobs
     list_recent_job(new_job_id, c.POOL_ID)
-    console.log(f"[bold][green]Finished listing current job for status check![/green]")
+    console.log(f"[magenta]Finished listing current job for status check!")
     sleep(1)
-    console.log(f'[bold][red]Done!') 
+    console.log(f'[bold red]Done!') 
 
